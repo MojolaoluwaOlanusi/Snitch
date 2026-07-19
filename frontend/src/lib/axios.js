fadaecf38f552b586e8c3753711dab9aa797f8bc
MojolaoluwaOlanusi <olanusimojola@gmail.com>
2026-07-19 08:10:12 +0100

    Used env in axiosinstance

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:4500/api",
+    baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 export default axiosInstance;
