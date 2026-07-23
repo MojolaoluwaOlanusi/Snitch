c9891c1646afac479375d6a6bea770310a4a13d7
MojolaoluwaOlanusi <olanusimojola@gmail.com>
2026-07-23 16:59:08 +0100

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client"
 import "./index.css";
 import App from "./App.jsx";
+import VConsole from 'vconsole';
 import {BrowserRouter} from "react-router-dom";
 import {DevSupport} from "@react-buddy/ide-toolbox";
 import {ComponentPreviews, useInitial} from "./dev/index.ts";
@@ -10,6 +11,10 @@ import { onLCP, onINP, onCLS } from "web-vitals";
 onLCP(console.log);
 onINP(console.log);
 onCLS(console.log);
+
+if (import.meta.env.DEV || window.location.search.includes('debug')) {
+    new VConsole();
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
